ef8d82e11f50eb9d25224755d98aaeb11b9c6634
filipchukdv <filipchukdv@gmail.com>
2023-04-13 18:20:31 +0300

    додано компонент Profile

---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,20 @@
+import css from './Statistics.module.css';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className="title"> {title}</h2>}
+
+      <ul className="stat-list">
+        {stats.map(stat => (
+          <li key={stat.id} className="item">
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Statistics;
